d70731b5c67cff50dab95fccd264514492180c34
hht <2459027626@qq.com>
2024-05-30 16:53:41 +0800

    字体渐变组件 修改颜色

--- examples/components/fontTitle.vue
+++ examples/components/fontTitle.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="">
-        <zy-fontTitle :fontText="'安昌街道安华村驻村团队智慧应用'" fontSize="50px" :textShadow="'1px 1px 1px #007acc'"  :background="'linear-gradient(to bottom,#fff, #337CB8)'"></zy-fontTitle>
-        <!-- #337CB8 -->
+    <div class="wrap">
+        <zy-fontTitle  :fontText="'安昌街道安华村驻村团队智慧应用'" fontSize="50px" :textShadow="'1px 1px 1px #007acc'"  
+        :background="'linear-gradient(#fff 20%,#fff 35%, #337CB8 100%)'"></zy-fontTitle>
+        <!-- :background="'linear-gradient(to bottom,#fff, #337CB8)'"></zy-fontTitle> -->
+
     </div>
 </template>
 
@@ -23,4 +25,9 @@ export default {
 };
 
 </script>
+<style>
+.wrap{
+    font-weight: 600;
+}
+</style>
 
